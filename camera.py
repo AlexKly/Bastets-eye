edf361885071b79c920cf9733503f1978e1ec83a
AlexKly <Klyukvanstalker@gmail.com>
2023-02-25 21:25:37 +0300

    try ov5640

--- camera.py
+++ camera.py
@@ -1,39 +1,39 @@
-import busio
-from adafruit_ov7670 import OV7670
+import busio, adafruit_ov5640
 from adafruit_blinka.board.raspberrypi import raspi_40pin
 
-GPIO2 = raspi_40pin.D2      # SCL
-GPIO3 = raspi_40pin.D3      # VS
-GPIO4 = raspi_40pin.D4      # PLK
-GPIO7 = raspi_40pin.D7      # PWDN
-GPIO8 = raspi_40pin.D8      # D0
-GPIO9 = raspi_40pin.D9      # RET
-GPIO10 = raspi_40pin.D10    # D1
-GPIO14 = raspi_40pin.D14    # SDA
-GPIO15 = raspi_40pin.D15    # HS
-GPIO17 = raspi_40pin.D17    # D7
-GPIO18 = raspi_40pin.D18    # XLK
-GPIO22 = raspi_40pin.D22    # D3
-GPIO23 = raspi_40pin.D23    # D6
-GPIO24 = raspi_40pin.D24    # D4
-GPIO25 = raspi_40pin.D25    # D2
-GPIO27 = raspi_40pin.D27    # D5
+GPIO2 = raspi_40pin.D2      # CMOS_SCL
+GPIO3 = raspi_40pin.D3      # CMOS_PCLK
+GPIO4 = raspi_40pin.D4      # CMOS_D5
+GPIO7 = raspi_40pin.D7      # CMOS_PWDN
+GPIO8 = raspi_40pin.D8      # CMOS_D3
+GPIO9 = raspi_40pin.D9      # CMOS_RESET
+GPIO10 = raspi_40pin.D10    # CMOS_D7
+GPIO14 = raspi_40pin.D14    # CMOS_SDA
+GPIO15 = raspi_40pin.D15    # CMOS_VSYNC
+GPIO17 = raspi_40pin.D17    # CMOS_D9
+GPIO18 = raspi_40pin.D18    # CMOS_D4
+GPIO22 = raspi_40pin.D22    # CMOS_D2
+GPIO23 = raspi_40pin.D23    # CMOS_D8
+GPIO24 = raspi_40pin.D24    # CMOS_HREF
+GPIO25 = raspi_40pin.D25    # CMOS_D6
+GPIO27 = raspi_40pin.D27    # CMOS_XCLK
 
 
 class CameraWrapper:
     def __init__(self, cam_size=(80, 60)):
         # Camera initialization and configuration:
         self.cam_bus = busio.I2C(scl=GPIO2, sda=GPIO14)    # Camera configuration interface (I2C)
-        self.cam = OV7670(
+        self.cam = adafruit_ov5640.OV5640(
             i2c_bus=self.cam_bus,
-            data_pins=[GPIO8, GPIO10, GPIO25, GPIO22, GPIO24, GPIO27, GPIO23, GPIO17],
-            clock=GPIO4,
-            vsync=GPIO3,
-            href=GPIO15,
-            mclk=GPIO18,
+            data_pins=[GPIO22, GPIO8, GPIO18, GPIO4, GPIO25, GPIO10, GPIO23, GPIO17], # D2, D3, D4, D5, D6, D7, D8, D9
+            clock=GPIO3,
+            vsync=GPIO15,
+            href=GPIO24,
             shutdown=GPIO7,
             reset=GPIO9,
+            mclk=GPIO27,
+            size=adafruit_ov5640.OV5640_SIZE_QQVGA
         )
         self.cam.size = cam_size
         # Temporary:
-        self.image_buf = bytearray(2 * self.cam.width * self.cam.height)
+        self.image_buf = bytearray(self.cam.capture_buffer_size)
